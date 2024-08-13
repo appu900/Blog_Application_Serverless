2dc33ed4019f35ca807c0c16e000f73d71713f45
pabitra <89703515+appu900@users.noreply.github.com>
2024-08-14 04:28:34 +0530

    added like and remove like end point

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -2,12 +2,22 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { decode, verify } from "hono/jwt";
-import { S3Client, PutObjectCommand, S3 } from "@aws-sdk/client-s3";
+import { Upload } from "@aws-sdk/lib-storage";
+import {
+  S3Client,
+  PutObjectCommand,
+  S3,
+  ObjectCannedACL,
+} from "@aws-sdk/client-s3";
 
 export const blogRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
+    AWS_ACCESS_KEY_ID: any;
+    AWS_SECRET_ACCESS_KEY: any;
+    AWS_BUCKET_NAME: any;
+    AWS_REGION: any;
   };
   Variables: {
     userId: string;
@@ -30,20 +40,33 @@ blogRouter.use("/*", async (c, next) => {
   }
 });
 
+// ** crate a post route
+
 blogRouter.post("/", async (c) => {
   const userId = c.get("userId");
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
-  // const s3Client = new S3Client({
-  //   region:c.env.
-  // })
+  const s3 = new S3Client({
+    credentials: {
+      accessKeyId: c.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: c.env.AWS_SECRET_ACCESS_KEY,
+    },
+    region: c.env.AWS_REGION,
+  });
 
   const form = await c.req.formData();
-  const file = form.get("file");
-  const title = form.get("title");
-  const content = form.get("content");
+  const file = form.get("img");
+  const postTitle = form.get("title");
+  const postContent = form.get("content");
+
+  if (postTitle === null || postContent === null) {
+    c.status(400);
+    return c.json({
+      error: "Content or title is required",
+    });
+  }
 
   if (!(file instanceof File)) {
     c.status(400);
@@ -55,25 +78,44 @@ blogRouter.post("/", async (c) => {
   const fileContent = new Uint8Array(arrayBuffer);
 
   const uploadParams = {
-    Bucket: "",
-    Key: "",
-    Body: "",
+    Bucket: c.env.AWS_BUCKET_NAME,
+    Key: `uploads/${fileName}`,
+    Body: fileContent,
     ContentType: file.type,
+    ACL: ObjectCannedACL.public_read,
   };
 
-  const post = await prisma.post.create({
-    data: {
-      title: body.title,
-      content: body.content,
-      authorId: userId,
-    },
-  });
+  try {
+    const upload = new Upload({
+      client: s3,
+      params: uploadParams,
+    });
+    await upload.done();
+    const fileUrl = `https://${c.env.AWS_BUCKET_NAME}.s3.${c.env.AWS_REGION}.amazonaws.com/uploads/${fileName}`;
 
-  return c.json({
-    ok: true,
-    id: post.id,
-  });
+    const response = await prisma.post.create({
+      data: {
+        content: postContent.toString(),
+        title: postTitle.toString(),
+        image: fileUrl,
+        authorId: userId,
+      },
+    });
+
+    c.status(201);
+    return c.json({
+      ok: true,
+      data: response,
+    });
+  } catch (error: any) {
+    c.status(500);
+    return c.json({
+      error: error.message,
+    });
+  }
 });
+
+// ** update a post
 
 blogRouter.put("/", async (c) => {
   const body = await c.req.json();
@@ -94,6 +136,8 @@ blogRouter.put("/", async (c) => {
   return c.json({ ok: true, id: post.id });
 });
 
+// ** get a post
+
 blogRouter.get("/:id", async (c) => {
   const blogId = c.req.param("id");
   const prisma = new PrismaClient({
@@ -113,6 +157,8 @@ blogRouter.get("/:id", async (c) => {
   }
 });
 
+// ** get all posts
+
 blogRouter.get("/bulk/all", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
@@ -123,12 +169,97 @@ blogRouter.get("/bulk/all", async (c) => {
       content: true,
       title: true,
       id: true,
+      likeCount:true,
       author: {
         select: {
-          name: true,
+          email: true,
         },
       },
     },
   });
   return c.json({ ok: true, posts: posts });
+});
+
+// ** like a post
+
+blogRouter.post("/like", async (c) => {
+  const userId = c.get("userId");
+  const postId = c.req.query("postId") as string;
+
+  if (!userId || !postId) {
+    c.status(400);
+    return c.json({
+      error: "postId is required",
+    });
+  }
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const existingLike = await prisma.like.findUnique({
+      where: {
+        postId_userId: {
+          postId: postId,
+          userId: userId,
+        },
+      },
+    });
+
+    if (existingLike) {
+      // ** if user already liked the post then just delete the like from like table and decrese like count in post model
+
+      await prisma.$transaction([
+        prisma.like.delete({
+          where: {
+            id: existingLike.id,
+          },
+        }),
+
+        prisma.post.update({
+          where: { id: postId },
+          data: { likeCount: { decrement: 1 } },
+        }),
+      ]);
+
+      c.status(201);
+      return c.json({
+        ok: true,
+      });
+    } else {
+     
+      // ** else create a like row and update the likes in post
+
+      await prisma.$transaction([
+        prisma.like.create({
+          data: {
+            postId: postId,
+            userId: userId,
+          },
+        }),
+        prisma.post.update({
+          where: {
+            id: postId,
+          },
+          data: {
+            likeCount: {
+              increment: 1,
+            },
+          },
+        }),
+      ]);
+
+      c.status(201);
+      return c.json({
+        ok: true,
+      });
+    }
+  } catch (error: any) {
+    c.status(500);
+    return c.json({
+      ok: false,
+      error: error.message,
+    });
+  }
 });
